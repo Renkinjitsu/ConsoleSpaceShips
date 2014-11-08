c288ed8f8b69cddbc8ec40e4ef28d5ac1ee951d9
dutchy <dutchy15@gmail.com>
2014-11-08 14:57:54 +0200

    added ship class.. work to be done

---
+++ hw2/Ship.h
@@ -0,0 +1,38 @@
+#ifndef _IO_UTILS_H_
+#define _IO_UTILS_H_
+
+typedef struct
+{
+	int x;
+	int y;
+}Ship_location_t;
+
+typedef struct
+{
+	int size_lines;
+	int size_columns;
+}Ship_size_t;
+
+class Ship
+{
+private:
+	Ship_location_t location;
+	Ship_size_t size;
+
+
+public:
+	Ship_location_t get_ship_location()
+	{
+		return location;
+	};
+
+	Ship(Ship_size_t ship_size, Ship_location_t ship_starting_location)
+	{
+		size = ship_size;
+		location = ship_starting_location;
+	};
+
+
+};
+
+#endif
